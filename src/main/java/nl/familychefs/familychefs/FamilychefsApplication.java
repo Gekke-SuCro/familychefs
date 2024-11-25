edd8129435ba63d6a758e8ac6ac40886d280a106
Gekke-SuCro <jayden.roeper@student.hu.nl>
2024-11-25 16:28:50 +0100

    Setup with Spring Initializr

---
+++ src/main/java/nl/familychefs/familychefs/FamilychefsApplication.java
@@ -0,0 +1,13 @@
+package nl.familychefs.familychefs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilychefsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilychefsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/familychefs/familychefs/FamilychefsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.familychefs.familychefs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilychefsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
