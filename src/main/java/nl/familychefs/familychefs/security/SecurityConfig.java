24136a14e8e8b38c19a5ff8d2b17954146952694
Gekke-SuCro <jayden.roeper@student.hu.nl>
2024-11-26 16:55:20 +0100

    Used BCryptPasswordEncoder for the in memory userdetails service.

--- src/main/java/nl/familychefs/familychefs/security/SecurityConfig.java
+++ src/main/java/nl/familychefs/familychefs/security/SecurityConfig.java
@@ -2,11 +2,14 @@ package nl.familychefs.familychefs.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,34 +17,33 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests
-                        // .requestMatchers("/", "/home").permitAll()
-                        .anyRequest().authenticated())
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll())
-                .logout((logout) -> logout.permitAll());
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 
-        return http.build();
-    }
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http.authorizeHttpRequests((requests) -> requests
+                                .requestMatchers("/api/v1/**").permitAll()
+                                .anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
+                return http.build();
+        }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User
-                .withUsername("user")
-                .password("user")
-                .roles("USER")
-                .build();
+        @Bean
+        public UserDetailsService userDetailsService() {
+                UserDetails user = User
+                                .withUsername("user")
+                                .password(passwordEncoder().encode("user"))
+                                .roles("USER")
+                                .build();
 
-        UserDetails admin = User
-                .withUsername("admin")
-                .password("admin")
-                .roles("ADMIN")
-                .build();
+                UserDetails admin = User
+                                .withUsername("admin")
+                                .password(passwordEncoder().encode("admin"))
+                                .roles("ADMIN")
+                                .build();
 
-        return new InMemoryUserDetailsManager(user, admin);
-    }
+                return new InMemoryUserDetailsManager(user, admin);
+        }
 }
