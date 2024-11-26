775eac262052e912a1acea35a186e1ce25208710
Gekke-SuCro <jayden.roeper@student.hu.nl>
2024-11-26 17:00:36 +0100

    Created UserController class
    UserController has get, post and put methods for users.

---
+++ src/main/java/nl/familychefs/familychefs/security/presentation/UserController.java
@@ -0,0 +1,32 @@
+package nl.familychefs.familychefs.security.presentation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    @GetMapping
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    public String getAllUsers(@RequestParam String param) {
+        return "List of all users";
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    public String saveUsers() {
+        return "Saved user";
+    }
+
+    @PutMapping
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    public String updateUsers() {
+        return "Updated user";
+    }
+}
