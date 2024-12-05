18349f3628b636e8fabb01eb1234bc3e272bad7b
Gekke-SuCro <jayden.roeper@student.hu.nl>
2024-12-06 00:58:43 +0100

    home page now displays the recipe card in a nice grid, inside a section container.

--- frontend/src/components/RecipeCard.jsx
+++ frontend/src/components/RecipeCard.jsx
@@ -4,9 +4,13 @@ import PropTypes from "prop-types";
 export const RecipeCard = ({ recipeData } ) => {
     return (
         <>
-            <div className="p-2 bg-gray-50 max-w-[256px]">
-                <span>{recipeData?.title}</span>
-                <p>{recipeData?.description}</p>
+            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow w-full h-full">
+                {/*> recipe img will come here */}
+
+                <div className="p-5">
+                    <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900">{recipeData?.title}</h5>
+                    <p className="mb-3 font-normal text-gray-700">{recipeData?.description}</p>
+                </div>
             </div>
         </>
     )
--- frontend/src/views/HomePage.jsx
+++ frontend/src/views/HomePage.jsx
@@ -6,25 +6,40 @@ export const HomePage = () =>  {
     const [recipesData, setRecipesData] = useState([]);
 
     useEffect(() => {
-        const fetchData = async function () {
-            const data = await RecipeAPI.getAll();
-            console.log(data);
-            setRecipesData(data)
-        }
+        // const fetchData = async function () {
+        //     const data = await RecipeAPI.getAll();
+        //     console.log(data);
+        //     setRecipesData(data)
+        // }
+        //
+        // fetchData().then(r => {});
 
-        fetchData().then(r => {});
+        const dummyData = [
+            {
+                id: 1,
+                description: "Heerlijke surinaamse nasy",
+                title: "Surinaamse Nasy",
+            },
+            {
+                id: 2,
+                description: "Frisse tonijnsalade, alsof het vers uit de zee komt.\"\n",
+                title: "Frisse Tonijnsalade",
+            },
+            {
+                id: 3,
+                description: "Knapperige panini met gesmolten kaas, tonijnsalade, saus en rijk belegd met olijven, pepers en kappertjes.",
+                title: "Pittige Panini Tonijn",
+            },
+        ];
+        setRecipesData(dummyData);
     }, []);
 
     return (
         <>
-            <h3>Recipes</h3>
-            <section>
-                <div className="recipes">
-                    {recipesData.map((recipeData) => (
-                        <>
-                            <RecipeCard recipeData={recipeData} />
-                        </>
-                    ))}
+            <h3 className="text-2xl font-bold text-gray-800 mb-8 text-center pt-12 pb-6">Recipes</h3>
+            <section className="pb-12">
+                <div className="container mx-auto px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7 justify-items-center">
+                    {recipesData.map((recipeData) => (<RecipeCard recipeData={recipeData} />))}
                 </div>
             </section>
         </>
