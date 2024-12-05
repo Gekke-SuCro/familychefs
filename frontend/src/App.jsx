0749d3c1dcf11ca6d9a3b0b566ceeb725ab267d2
Gekke-SuCro <jayden.roeper@student.hu.nl>
2024-12-05 23:15:54 +0100

    React router setup with App.jsx as main layout component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,15 @@ import viteLogo from '/vite.svg'
 import './assets/styles/App.css'
 import './views/HomePage'
 import {HomePage} from "./views/HomePage.jsx";
+import {BrowserRouter, Outlet, Route, Routes} from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <main>
-            <HomePage />
-        </main>
+        <p>App Layout</p>
+        <Outlet />
     </>
   )
 }
