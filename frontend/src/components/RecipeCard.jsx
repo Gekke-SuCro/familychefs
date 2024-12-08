665df6d4a3ac65da67e611baf367bc698b9ee566
Gekke-SuCro <jayden.roeper@student.hu.nl>
2024-12-08 17:35:37 +0100

    Updated recipe cards to display new recipe data.

--- frontend/src/components/RecipeCard.jsx
+++ frontend/src/components/RecipeCard.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export const RecipeCard = ({ recipeData } ) => {
+import {ClockIcon} from "@heroicons/react/16/solid/index.js";
+
+export const RecipeCard = ({recipeData}) => {
     return (
         <>
-            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow w-full h-full">
+            <div
+                className="max-w-sm bg-white border border-gray-200 rounded-lg shadow w-full h-full p-8 flex flex-col justify-between">
                 {/*> recipe img will come here */}
 
-                <div className="p-5">
-                    <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900">{recipeData?.title}</h5>
-                    <p className="mb-3 font-normal text-gray-700">{recipeData?.description}</p>
+                <div className="flex flex-col gap-8 mb-4">
+                    {/* Card header */}
+                    <p className="p-2 font-black text-sm text-center text-gray-700 bg-gray-200 rounded-lg">
+                        {recipeData?.username}
+                    </p>
+
+                    {/* Card content */}
+                    <div>
+                        <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900">{recipeData?.title}</h5>
+                        <p className="mb-3 font-normal text-gray-700">{recipeData?.description}</p>
+                    </div>
+                </div>
+
+                {/* Card footer */}
+                <div className="flex flex-wrap gap-4">
+                    <div className="flex items-center gap-2 bg-gray-200 rounded-lg px-5 py-2">
+                        <ClockIcon className={"size-5 text-gray-700"} />
+                        <span className="font-normal text-gray-700">{recipeData?.prepareTime}</span>
+                    </div>
                 </div>
             </div>
         </>
@@ -21,5 +40,7 @@ RecipeCard.propTypes = {
         id: PropTypes.number.isRequired,
         title: PropTypes.string.isRequired,
         description: PropTypes.string.isRequired,
+        prepareTime: PropTypes.number.isRequired,
+        username: PropTypes.string.isRequired
     }).isRequired
 }
--- frontend/src/utils/DummyData.js
+++ frontend/src/utils/DummyData.js
@@ -3,15 +3,21 @@ export const dummyRecipes = [
         id: 1,
         description: "Heerlijke surinaamse nasy",
         title: "Surinaamse Nasy",
+        prepareTime: 320,
+        username: "John Doe",
     },
     {
         id: 2,
         description: "Frisse tonijnsalade, alsof het vers uit de zee komt.\"\n",
         title: "Frisse Tonijnsalade",
+        prepareTime: 320,
+        username: "user000915288",
     },
     {
         id: 3,
         description: "Knapperige panini met gesmolten kaas, tonijnsalade, saus en rijk belegd met olijven, pepers en kappertjes.",
         title: "Pittige Panini Tonijn",
+        prepareTime: 320,
+        username: "user95828639",
     },
 ];
